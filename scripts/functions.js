81f894ceff2e0d0d1c4c88ec370dfa92c3e7d3b2
Kessvan Cedeño <kessvan.one@gmail.com>
2022-09-20 01:41:30 +0200

    Actividad 1.2-Frases

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,33 +1,39 @@
 'use strict'
 
 function letras(cadena) {
-
+	return cadena.length;
 }
 
 function palabras(cadena) {
-
+	return cadena.split(" ").length;
 }
 
 function maysc(cadena) {
-
+	return cadena.split(" ").map(paraula => paraula = paraula.toLocaleUpperCase()).join(" ");
 }
 
 function titulo(cadena) {
-
+	let paraules = cadena.split(" ");
+	return paraules.map(paraula => paraula = paraula[0].toLocaleUpperCase() + paraula.substr(1)).join(" ");
 }
 
 function letrasReves(cadena) {
-
+	let letras = cadena.split("");
+	return letras.reverse().join("");
 }
 
 function palabrasReves(cadena) {
-
+	let paraules = cadena.split(" ");
+	return paraules.reverse().join(" ");
 }
 
 function palindromo(cadena) {
-
+	cadena = cadena.split(" ").join("").toLocaleLowerCase();
+	let cadenaReves = letrasReves(cadena).split(" ").join("").toLocaleLowerCase();
+	return cadena == cadenaReves;
 }
 
+/**
 module.exports = {
 	letras,
 	palabras,
@@ -37,3 +43,4 @@ module.exports = {
 	palabrasReves,
 	palindromo
 }
+*/
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,11 @@
 'use strict'
 
+let cadena = prompt("Introduzca una frase:");
 
+console.log("Frase: \"" + cadena + "\"");
+console.log(letras(cadena) + " letras y " + palabras(cadena) + " palabras");
+console.log(maysc(cadena));
+console.log(titulo(cadena));
+console.log(letrasReves(cadena));
+console.log(palabrasReves(cadena));
+console.log(palindromo(cadena) ? "Sí" : "No" + " es un palíndromo")
